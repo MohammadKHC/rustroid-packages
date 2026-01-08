bec806fd14ec3c3a2aac3c28da0e3811a5cd5d8d
MohammedKHC <mohammedkhc@outlook.com>
2026-01-08 15:37:55 +0200

    test3

--- src/main.kt
+++ src/main.kt
@@ -4,50 +4,21 @@ val termuxPackagesDir = File("termux-packages-master")
 
 fun main(args: Array<String>) {
     runCommand("curl -sL https://github.com/termux/termux-packages/archive/master.tar.gz | tar -xz")
-    setupEnvironment()
-    $$"""
-        # these need to be unset a second time again for ./build-package.sh
-        unset NDK ANDROID_HOME
-        ./build-package.sh -I -C -a "${{ matrix.target_arch }}" "${packages[@]}"
-    """.trimIndent()
-
     buildPackages(listOf("bash"), args.first())
-}
-
-private fun setupEnvironment() {
-    require(termuxPackagesDir.isDirectory)
-    if (System.getenv("CI") == null) {
-        println("Ignoring setupEnvironment to not mess up your system.")
-        return
-    }
-    runCommand(
-        $$"""
-            ./scripts/setup-ubuntu.sh
-            # need to unset these for setup-android-sdk.sh.
-            unset NDK ANDROID_HOME
-            ./scripts/setup-android-sdk.sh
-            rm -f ${HOME}/lib/ndk-*.zip ${HOME}/lib/sdk-*.zip
-            sudo apt install ninja-build
-            ./scripts/free-space.sh
-        """.trimIndent(),
-        termuxPackagesDir
-    )
 }
 
 private fun buildPackages(packages: List<String>, arch: String) {
     require(termuxPackagesDir.isDirectory)
     runCommand(
-        """
-            # these need to be unset a second time again for ./build-package.sh
-            unset NDK ANDROID_HOME
-            ./build-package.sh -a $arch ${packages.joinToString(" ")}
-        """.trimIndent(),
+        "./scripts/run-docker.sh ./build-package.sh -a $arch ${packages.joinToString(" ")}",
+        "build-package",
         termuxPackagesDir
     )
 }
 
 private fun runCommand(
     command: String,
+    label: String? = null,
     directory: File? = null
 ) = ProcessBuilder()
     .command("sh", "-c", command)
@@ -55,7 +26,7 @@ private fun runCommand(
     .redirectErrorStream(true)
     .start().apply {
         inputStream.bufferedReader().forEachLine {
-            println("${command.substringBefore(' ')}: $it")
+            println("$label: $it")
         }
         waitFor().let {
             if (it != 0) error("$command got exit code: $it")
